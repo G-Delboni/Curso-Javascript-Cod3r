972d0cc3c726c39d14fcc21fbc372b524962d870
G-Delboni <ygabrieldelboni.contato@gmail.com>
2023-01-05 14:49:26 -0300

    estudando objetos

---
+++ Fundamentos/objeto.js
@@ -0,0 +1,20 @@
+const prod1 = {}
+prod1.nome = "Celular Iphone 13" // nome criado dinamicamente
+prod1.preço = "6998.90"
+prod1['armazenamento'] = "128 GB"
+
+console.log(prod1)
+
+const prod2 = {
+    nome: "Teclado mecânico",
+    marca: "Redragon",
+    modelo: "Kumara K552",
+    switch: {
+        marca: "Otemu",
+        tipo: "Brown",
+    }
+}
+
+// '{"nome": "abobora", "preço: 10.99"}' - exemplo de json
+
+console.log(prod2)
