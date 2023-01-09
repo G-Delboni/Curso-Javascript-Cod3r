519007ee5824d8b2548c5990644711e07c0e368c
G-Delboni <ygabrieldelboni.contato@gmail.com>
2023-01-09 16:36:05 -0300

    estudos de funcao 2

--- Fundamentos/funcao2.js
+++ Fundamentos/funcao2.js
@@ -1,1 +1,19 @@
-const imprimirSoma = function
+const imprimirSoma = function (a, b) {
+    console.log(a + b)
+}
+
+imprimirSoma(2, 3)
+
+//Armazenando uma função arrow em uma variável
+
+const soma = (a,b) => {
+    return a + b
+}
+
+//return implicito - executa uma única linha
+
+const subtrair = (a,b) => a-b
+console.log(subtrair(2, 3))
+
+const imprimir2 = a => console.log(a)
+imprimir2(3)
