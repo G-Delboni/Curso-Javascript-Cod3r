03576a910531cbaa01e412768cf473ee6c57ab4b
G-Delboni <ygabrieldelboni.contato@gmail.com>
2023-01-10 00:51:29 -0300

    estudando let loop

---
+++ Fundamentos/letloop1.js
@@ -0,0 +1,5 @@
+for(let i = 0; i < 10; i++) { // primeira parte é a declaração, segunda é a condição, e terceira é o incremento.
+    console.log(i)
+}
+
+console.log('i = ', i) // está fora do bloco (let - escopo de bloco)
