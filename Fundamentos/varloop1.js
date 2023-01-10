09634f2b4dc6ebbb3b487cb0ca1f0d9e1e1e6d42
G-Delboni <ygabrieldelboni.contato@gmail.com>
2023-01-10 00:30:04 -0300

    estudando loop com var

---
+++ Fundamentos/varloop1.js
@@ -0,0 +1,5 @@
+for(var i = 0; i < 10; i++) { // primeira parte é a declaração, segunda é a condição, e terceira é o incremento.
+    console.log(i)
+}
+
+console.log('i = ', i) // valor que faz sair do laço
