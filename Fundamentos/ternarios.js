947761b76c98aa0dd2ae16a3cafb856c50b12d77
G-Delboni <ygabrieldelboni.contato@gmail.com>
2023-01-12 18:02:03 -0300

    estudando operadores ternarios.js

---
+++ Fundamentos/ternarios.js
@@ -0,0 +1,6 @@
+const resultado = nota => nota>=7 ? 'Aprovado': 'Reprovado' // arrow function, expressão relacional, se for verdadeiro retorna 'Aprovado', se for falso retorna 'Reprovado'
+
+console.log(resultado(8))
+console.log(resultado(4))
+
+
