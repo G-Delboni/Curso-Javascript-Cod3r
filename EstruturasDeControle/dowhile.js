2e1f9638a89940f618b3cf1bdc486d178a6f21eb
G-Delboni <ygabrieldelboni.contato@gmail.com>
2023-01-14 20:18:09 -0300

    estudando do/while

---
+++ EstruturasDeControle/dowhile.js
@@ -0,0 +1,13 @@
+function gerarInteiro(min, max) {
+    const valor = Math.random() * (max-min) + min
+    return Math.floor(valor)
+}
+
+let opcao = 0
+
+do{
+    opcao = gerarInteiro(-1, 10)
+    console.log(opcao)
+} while(opcao != -1) 
+
+console.log('Fim')
