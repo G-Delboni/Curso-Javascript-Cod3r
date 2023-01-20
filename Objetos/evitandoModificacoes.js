04dc5ae039f827c565171941f76d71a77a916fdd
G-Delboni <ygabrieldelboni.contato@gmail.com>
2023-01-20 18:07:27 -0300

    estudando objetos

---
+++ Objetos/evitandoModificacoes.js
@@ -0,0 +1,33 @@
+// Object.preventExtensions -> Permite alterar atributos e excluir, mas não adicionar novos
+
+const produto = Object.preventExtensions({ 
+    nome: 'qualquer',
+    preco: 1.99,
+    tag: 'promoção'
+}) 
+
+console.log('Extensível:',Object.isExtensible(produto))
+
+produto.nome = 'Borracha'
+produto.descricao = 'Borracha escolar'
+delete produto.tag
+
+console.log(produto)
+
+// Object.seal -> Permite alterar mas não excluir nem adicionar novos
+
+const pessoa = {
+    nome: 'Juliana',
+    idade: 35
+}
+
+Object.seal(pessoa)
+console.log('Selado:', Object.isSealed(pessoa))
+
+pessoa.sobrenome = 'Silva'
+delete pessoa.nome
+pessoa.idade = 29
+
+console.log(pessoa)
+
+// Object.freeze = selado + valores constantes
