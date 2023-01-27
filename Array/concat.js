9e8b94e30224414b3f10b71bc03be0d3663456f1
G-Delboni <ygabrieldelboni.contato@gmail.com>
2023-01-26 21:17:31 -0300

    estudando concat

---
+++ Array/concat.js
@@ -0,0 +1,7 @@
+const filhas = ['Valeska','Cibalena']
+const filhos = ['Washinton','Weskley']
+const todos = filhas.concat(filhos)
+
+console.log(todos)
+
+console.log([].concat([1,2], [3,4], 5, [[6,7,8,9]]))
